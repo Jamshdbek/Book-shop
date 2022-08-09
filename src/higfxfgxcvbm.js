db2ad082a68f404957ff16ad3f211b0595564c41
Yoldoshev Abror <97498734+abrordevv@users.noreply.github.com>
2022-08-10 00:46:27 +0500

    Yangi file qoshdim test uchun

---
+++ src/higfxfgxcvbm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Higfxfgxcvbm(props) {
+    return (
+        <div>
+            lkhgfdhbjml,;"Lhgdhf
+        </div>
+    );
+}
+
+export default Higfxfgxcvbm;
