3174fd3f3663e5b44cdae1ead37b1b1d632cdf7e
Yoldoshev Abror <97498734+abrordevv@users.noreply.github.com>
2022-08-10 10:18:28 +0500

    noot

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./routes/Cart";
 import Layout from "./routes/Layout";
 import Shop from "./routes/Shop";
+// umumiy scss link ulnagan va hamma sasslar shunga inport qilngan
 import './styles/styles.scss'
+//bootstrap react css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -1,49 +1,13 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector} from "react-redux";
-import { addBook } from "../actions/bookActions";
+import React from "react";
+
 import Books from "../components/Books";
+import FormInput from "../components/FormInput";
 
 function Shop() {
-  let dispatch = useDispatch();
-  const [book, setBook] = useState({});
-  const handleChange = (e) => {
-    setBook({ ...book, [e.target.name]: e.target.value });
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addBook(book));
-    setBook({});
-  };
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          value={book.title || ""}
-          placeholder="Title"
-        />
-        <input
-          type="text"
-          name="author"
-          onChange={handleChange}
-          value={book.author || ""}
-          placeholder="Author"
-        />
-        <input
-          type="text"
-          name="price"
-          onChange={handleChange}
-          value={book.price || ""}
-          placeholder="Price"
-        />
-        <button>Submit</button>
-      </form>
-
-      <Books />
-    </div>
-  );
+    return (<div>
+            <FormInput/>
+            <Books/>
+        </div>);
 }
 
 export default Shop;
