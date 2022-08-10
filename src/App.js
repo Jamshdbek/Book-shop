3c13b518526f4288183b6fdd9b159c24fc918b03
Yoldoshev Abror <97498734+abrordevv@users.noreply.github.com>
2022-08-10 12:22:49 +0500

    yangi elemetlar qoshildi

--- src/App.js
+++ src/App.js
@@ -7,14 +7,20 @@ import Shop from "./routes/Shop";
 import './styles/styles.scss'
 //bootstrap react css
 import 'bootstrap/dist/css/bootstrap.min.css';
+import About from "./routes/About";
+import Contact from "./routes/Contact";
+import Category from "./routes/Category";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Shop />}></Route>
-          <Route path="cart" element={<Cart />}></Route>
+          <Route index element={<Shop/>}/>
+          <Route path='about' element={<About/>}/>
+          <Route path='contact' element={<Contact/>}/>
+          <Route path='category' element={<Category/>}/>
+          <Route path="cart" element={<Cart/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
