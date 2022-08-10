3abdf3ad441e8d79ade8da86fd5099f3cbca216c
Jamshidbek <jamshidbekd664@gmail.com>
2022-08-10 12:28:55 +0300

    cards

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,26 +1,49 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { removeBook } from '../actions/bookActions'
-;
+import { removeBook } from "../actions/bookActions";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+
 function BookCard(props) {
   const dispatch = useDispatch();
   const book = props.book;
 
   const deleteBook = () => {
-    dispatch(removeBook(book.id))
-  }
+    dispatch(removeBook(book.id));
+  };
 
-  const handleBuy = () =>{
+  const handleBuy = () => {
     // dispatch(buyBook(book.id))
-  }
+  };
 
   return (
-    <div className="bookCard">
-      <h3>{book.title}</h3>
+    <div >
+      {/* <h3>{book.title}</h3>
       <h4>{book.author}</h4>
       <p>{book.price}</p>
       <button onClick={deleteBook}>Remove</button>
-      <button onClick={handleBuy}>Buy</button>
+      <button onClick={handleBuy}>Buy</button> */}
+
+      <Card className="bookCard" style={{ width: "18rem" }}>
+        <Card.Img
+          variant="top"
+          src="https://avatars.mds.yandex.net/i?id=86867fcac3a93054e494bdbcd7f7b423-6088960-images-thumbs&n=13"
+        />
+        <Card.Body>
+          {/* <Card.Title>{book.title}</Card.Title> */}
+          <Card.Text>{book.author}</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          {/* <ListGroup.Item>Cras justo odio</ListGroup.Item> */}
+          {/* <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item> */}
+          <ListGroup.Item>
+            {" "}
+            <p>{book.price}</p>
+          </ListGroup.Item>
+          <button onClick={deleteBook}>Remove</button>
+          <button onClick={handleBuy}>Buy</button>
+        </ListGroup>
+      </Card>
     </div>
   );
 }
--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import BookCard from './BookCard';
-
+import "../styles/catd.scss"
 function Books() {
   const books = useSelector(state=> state.books);
   return (
-    <div>
+    <div className='body_card'>
       {books.map((book)=>{
         return <BookCard key={book.id} book={book}/>
       })}
